b758b770737303d74460d4e793ab71261b7dfc9d
Faizan <alifaizan15245@gmail.com>
2025-04-03 14:39:08 +0530

    fix error on build time

--- app/api/auth/update-password/route.ts
+++ app/api/auth/update-password/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../lib/prisma"; // Adjust path as needed
+import prisma from "@/lib/prisma"; // Adjust path as needed
 import bcrypt from "bcryptjs";
 
 export async function POST(req: NextRequest) {
